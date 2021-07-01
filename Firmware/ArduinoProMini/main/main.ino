68ef2de2c30a4c4d0fb2c20af330cd1720ec5e52
HarveyBates <phenobottle@gmail.com>
2021-07-01 16:47:48 +1000

    atmega328 needs RX <-> TX swap, button leads chopped, but LoRaWAN and SD Card working

--- Firmware/ArduinoProMini/main/main.ino
+++ Firmware/ArduinoProMini/main/main.ino
@@ -2,20 +2,19 @@
 // User Imports
 
 #include "coms.h"
+#include "memorycard.h"
 
-Scale scale; // Construct scale class, needed for HX711 library
+//Scale scale; // Construct scale class, needed for HX711 library
 
 void setup(){
   Serial.begin(9600);
   Communications::init();
+  MemoryCard::init();
 }
 
 void loop(){
   while(!Communications::check_state()){
     os_runloop_once();
     Serial.println("Working");
-    delay(1000);
   }
-
-  
 }
---
+++ Firmware/ArduinoProMini/main/memorycard.h
@@ -0,0 +1,20 @@
+/* 
+ * Memory Card functions for reading and writing data.
+ */
+  
+#ifndef MEMORY_CARD
+#define MEMORY_CARD
+
+#include "Arduino.h"
+#include <SD.h>
+#include <SPI.h>
+
+class MemoryCard {
+  
+public:
+  static void init();
+  static void write_data(const char* fileName, float* xValues, float* yValues, unsigned int size, unsigned long captureTime);
+  static void read_data(const char* fileName);
+};
+
+#endif
